2efce3aec370142d30471c5dd0e3c8ae2fa07177
kuhoo <kuhoon.kwon@rwth-aachen.de>
2022-11-18 14:58:34 +0100

    Sowohl sol103 als auch sol145 funktionieren ohne Probleme, und ich schreibe einen Bericht über die Ergebnisse, während ich die Daten mit dem Original abgleiche.

--- plot_vg_vf.py
+++ plot_vg_vf.py
@@ -2,6 +2,11 @@ from nastran.post.f06 import read_f06
 from nastran.post.flutter import get_critical_roots, join_flutter_pages, flutter_pages_to_df
 from nastran.post.plots import plot_vf_vg
 import matplotlib.pyplot as plt
+# how many modes want to print
+# Nastran always adds 6 more modes automatically.
+# There is no need to consider the 6 modes other than the mode that requested output.
+# can change mm/s to m/s or others
+# C:\Users\kuhoo\AppData\Local\Programs\Python\Python310\Lib\site-packages\nastran\post\plots.py
 
 res = read_f06('sol145_adddlm_f025_coupled.f06')
 
@@ -9,8 +14,8 @@ pages = join_flutter_pages(res.flutter)
 df = flutter_pages_to_df(pages)
 get_critical_roots(df)
 
-p = plot_vf_vg(df, modes=(1, 3, 4, 6, 7, 9, 10, 12, 15, 16, 17)) # how many modes want to print, if you want to all, del modes=(3,7)
+p = plot_vf_vg(df, modes=(1, 3, 4, 6, 7, 9, 10, 12))
 plt.title('FLUTTER ANALYSIS : Ref2020_DLM', pad=160)
-plt.xlabel('Equivalent Airspeed (mm/s)')
+plt.xlabel('Equivalent Airspeed (m/s)')
 plt.ylabel('                                                      DAMPING                       FREQUENCY(Hz)')
 plt.show()
